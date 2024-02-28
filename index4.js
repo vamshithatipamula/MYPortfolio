63f0362714978183ddcd0016f5e6e2a417115cdb
vamshithatipamula <131571120+vamshithatipamula@users.noreply.github.com>
2024-02-28 10:33:53 +0530

    Add files via upload

--- index4.js
+++ index4.js
@@ -7,8 +7,8 @@ let aboutbtn=document.querySelector(".aboutbtn");
 let resumebtn=document.querySelector(".resumebtn");
 let portfoliobtn=document.querySelector(".portfoliobtn");
 let contactbtn=document.querySelector(".contactsbtn");
-let showbtn=document.querySelector(".showcontacts");
-let forsmallbtn=document.querySelector(".forsmallsizebtn")
+let showbtn=document.querySelector(".show");
+// let forsmallbtn=document.querySelector(".show")
 let inf=document.querySelector(".inf");
 
 
@@ -39,53 +39,23 @@ contactbtn.addEventListener("click",()=>{
     portfoliopage.classList.add("hide");
     contactpage.classList.remove("hide");
 })
-let secondinf=document.querySelector(".secondinf");
+let another=document.querySelector(".anotherdetailbox");
 let conts="close";
 showbtn.addEventListener("click",()=>{
   if(conts==="close"){
     conts="open";
+    anotherdetailbox.style.display="block";
     console.log("dhs")
-    secondinf.style.display="flex";
+    
   }
   else{
     conts="close";
     console.log("dde");
-    secondinf.style.display="none"
-  }
-})
-
-
-
-forsmallbtn.addEventListener("click",()=>{
-  if(conts==="close"){
-    conts="open";
-    console.log("dhs")
-    secondinf.style.display="flex";
-  }
-  else{
-    conts="close";
-    console.log("dde");
-    secondinf.style.display="none"
+    another.style.display="none";
   }
 })
 
 
 
 
-const myDiv = document.getElementsByClassName('full');
-// const toggleButton = document.getElementById('toggle-button');
 
-// Create a media query matcher for small screens
-const mediaQuery = window.matchMedia('(max-width: 768px)'); // Customize the breakpoint as needed
-
-// Function to handle show/hide behavior
-const toggleDiv = () => {
-  if (mediaQuery.matches) { // Check if small screen
-    myDiv.style.height= window.innerHeight; // Toggle visibility on click
-  } 
-};
-
-window.addEventListener('resize', toggleDiv);
-
-// Initial state based on screen size
-toggleDiv();
